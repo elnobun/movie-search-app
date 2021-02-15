7b352d064dcea68e87829731e30b48859d90ce3b
elnobun <elnobun@gmail.com>
2021-02-15 23:34:09 +0000

    Created function to fetch data from API.

--- src/component/SearchMovies.js
+++ src/component/SearchMovies.js
@@ -1,6 +1,28 @@
+import getMoviesFromAPI from '../services/data'
+
 export default function SearchMovies () {
+
+    // Create function to call data from the movies API
+    const searchMovies = async (e) => {
+        e.preventDefault()
+        console.log('Submitted')
+
+        const query = 'Jurassic Park'
+
+        const url = getMoviesFromAPI(query)
+
+        try {
+            const res = await fetch(url)
+            const data = await res.json()
+
+            console.log(data)
+        } catch (e) {
+            console.error('error')
+        }
+    }
+
     return (
-        <form className="form">
+        <form className="form" onSubmit={searchMovies}>
             <label htmlFor="query" className="form__label">
             </label>
             <input type="text"
