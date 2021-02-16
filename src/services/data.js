419cac9823d0212ba42075ccb33c91207087792c
elnobun <elnobun@gmail.com>
2021-02-16 13:35:43 +0000

    Created hooks and card component to keep track of input and data.

--- src/services/data.js
+++ src/services/data.js
@@ -1,6 +1,5 @@
-const getMoviesFromAPI = (query) => {
-    return `https://api.themoviedb.org/3/movie/550?
-    api_key=522253dc073636e619fead2593d30366&language=en-US&query=${query}&page=1&include_adult=false`
+const fetchMoviesFromAPI = (query) => {
+    return `https://api.themoviedb.org/3/search/movie?api_key=522253dc073636e619fead2593d30366&language=en-US&query=${query}&page=1&include_adult=false`
 }
 
-export default getMoviesFromAPI
+export default fetchMoviesFromAPI
